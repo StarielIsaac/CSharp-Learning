438d1dfeecaab38dca71c8db64669a3da13c6afe
Stariel Isaac <starielisaac@gmail.com>
2023-07-03 16:50:44 -0300

    feat: implicit cast and explicit cast

---
+++ Fundamentos-e-Recursos-do-C#/Variáveis-Condicionais-e-Loops/cast_implicito_e_explicito/Program.cs
@@ -0,0 +1,33 @@
+﻿public class Program
+{
+  public static void Main()
+  {
+    int intResult = 5 / 2;
+
+    Console.WriteLine(intResult);
+  }
+}
+
+public class Program1
+{
+  public static void Main()
+  {
+    int someIntNumber = 51;
+    long longNumberCast = someIntNumber;
+
+    Console.WriteLine(longNumberCast);
+  }
+}
+
+public class Program2
+{
+  public static void Main()
+  {
+    long someLongNumber = 516144564564654;
+    int intNumber = Convert.ToInt32(someLongNumber);
+    Console.WriteLine(intNumber);
+  }
+}
+
+// Conversão de string para números
+
