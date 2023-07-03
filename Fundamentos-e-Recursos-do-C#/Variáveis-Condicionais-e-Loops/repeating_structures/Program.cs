27b4b779c21d2947b209602df3f37033d323ee61
Stariel Isaac <starielisaac@gmail.com>
2023-07-03 19:02:43 -0300

    feat: created for and foreach

--- Fundamentos-e-Recursos-do-C#/Variáveis-Condicionais-e-Loops/repeating_structures/Program.cs
+++ Fundamentos-e-Recursos-do-C#/Variáveis-Condicionais-e-Loops/repeating_structures/Program.cs
@@ -1,4 +1,5 @@
-﻿var count = 0;
+﻿//while
+var count = 0;
 while (count <= 10)
 {
     Console.WriteLine("count " + count);
@@ -8,6 +9,8 @@ while (count <= 10)
 // A instrução de repetição do/while fará primeiro a execução de um bloco de 
 // instruções para depois avaliar a expressão booleana especificada, e quando 
 // essa expressão for verdadeira, voltará ao início do bloco.
+
+//do/while
 var counter = 0;
 do
 {
@@ -15,3 +18,17 @@ do
     count++;
 }
 while (count < 10);
+
+//For
+for (int number = 0; count < 3; number++)
+{
+    Console.WriteLine("numero: " + number);
+}
+
+
+//Foreach
+string[] heroes = { "Hulk", "Thor", "Loki" };
+foreach (string hero in heroes)
+{
+    Console.WriteLine(hero);
+}
