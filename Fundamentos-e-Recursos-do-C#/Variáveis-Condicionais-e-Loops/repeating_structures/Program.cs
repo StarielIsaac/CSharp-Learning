b1624cc7750cb075221519a0dd95bf1d5b0a918d
Stariel Isaac <starielisaac@gmail.com>
2023-07-03 18:52:07 -0300

    feat: created repeating structures

--- Fundamentos-e-Recursos-do-C#/Variáveis-Condicionais-e-Loops/repeating_structures/Program.cs
+++ Fundamentos-e-Recursos-do-C#/Variáveis-Condicionais-e-Loops/repeating_structures/Program.cs
@@ -1,1 +1,17 @@
-﻿
+﻿var count = 0;
+while (count <= 10)
+{
+    Console.WriteLine("count " + count);
+    count++;
+}
+
+// A instrução de repetição do/while fará primeiro a execução de um bloco de 
+// instruções para depois avaliar a expressão booleana especificada, e quando 
+// essa expressão for verdadeira, voltará ao início do bloco.
+var counter = 0;
+do
+{
+    Console.WriteLine("count " + counter);
+    count++;
+}
+while (count < 10);
