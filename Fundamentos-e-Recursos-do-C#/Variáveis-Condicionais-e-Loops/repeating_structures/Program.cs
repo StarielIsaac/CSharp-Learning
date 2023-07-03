71d47314b89d0bb479c473d71124371ececa53d3
Stariel Isaac <starielisaac@gmail.com>
2023-07-03 19:19:38 -0300

    feat: created continue

--- Fundamentos-e-Recursos-do-C#/Variáveis-Condicionais-e-Loops/repeating_structures/Program.cs
+++ Fundamentos-e-Recursos-do-C#/Variáveis-Condicionais-e-Loops/repeating_structures/Program.cs
@@ -1,8 +1,8 @@
 ﻿//while
 var count = 0;
 while (count <= 10)
-    Console.WriteLine("count " + count);
-    count++;
+  Console.WriteLine("count " + count);
+count++;
 
 // A instrução de repetição do/while fará primeiro a execução de um bloco de 
 // instruções para depois avaliar a expressão booleana especificada, e quando 
@@ -12,19 +12,29 @@ while (count <= 10)
 var counter = 0;
 do
 {
-    Console.WriteLine("count " + counter);
-    count++;
+  Console.WriteLine("count " + counter);
+  count++;
 }
 while (count < 10);
 
 //For
 for (int number = 0; count < 3; number++)
-    Console.WriteLine("numero: " + number);
+  Console.WriteLine("numero: " + number);
 
 
 
 //Foreach
 string[] heroes = { "Hulk", "Thor", "Loki" };
 foreach (string hero in heroes)
-    Console.WriteLine(hero);
+  Console.WriteLine(hero);
 
+
+int[] votes = new int[] { 1, 3, 5, 4, 1, 3, 1, 2 };
+var countAll = 0;
+foreach (var vote in votes)
+{
+  if (vote > 3)
+    continue;
+  countAll++;
+}
+Console.WriteLine(countAll + 'votos válidos');
