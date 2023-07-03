86dd05fab2f0ee19673fc1412278837967ee4a4e
Stariel Isaac <starielisaac@gmail.com>
2023-07-03 19:04:42 -0300

    feat: removing unnecessary keys

--- Fundamentos-e-Recursos-do-C#/Variáveis-Condicionais-e-Loops/repeating_structures/Program.cs
+++ Fundamentos-e-Recursos-do-C#/Variáveis-Condicionais-e-Loops/repeating_structures/Program.cs
@@ -1,10 +1,8 @@
 ﻿//while
 var count = 0;
 while (count <= 10)
-{
     Console.WriteLine("count " + count);
     count++;
-}
 
 // A instrução de repetição do/while fará primeiro a execução de um bloco de 
 // instruções para depois avaliar a expressão booleana especificada, e quando 
@@ -21,14 +19,12 @@ while (count < 10);
 
 //For
 for (int number = 0; count < 3; number++)
-{
     Console.WriteLine("numero: " + number);
-}
+
 
 
 //Foreach
 string[] heroes = { "Hulk", "Thor", "Loki" };
 foreach (string hero in heroes)
-{
     Console.WriteLine(hero);
-}
+
