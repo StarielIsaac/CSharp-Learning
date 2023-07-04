2121e329b1fa0fdd2223c523b0a41d1c6019d240
Stariel Isaac <starielisaac@gmail.com>
2023-07-04 18:48:14 -0300

    feat: created try-catch and exceptions

--- Fundamentos-e-Recursos-do-C#/Arrays-Funções-e-Exceções/arrays_e_functions/examples-exceptions/Program.cs
+++ Fundamentos-e-Recursos-do-C#/Arrays-Funções-e-Exceções/arrays_e_functions/examples-exceptions/Program.cs
@@ -1,2 +1,53 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿//Em C#, as exceções podem ser geradas pelo CLR (Common Language Runtime) do .NET, 
+//ou pelo código do próprio sistema
+
+//sintaxe do try-catch
+try
+{
+  // Código a ser executado
+}
+catch
+{
+  // Caso aconteça uma exceção no código dentro do bloco try, aqui vai ser requisitado!
+}
+
+//exemplo mais completo (IndexOutOfRangeException, Exception) 
+string[] chemicalProduct = new string[3];
+
+try
+{
+  chemicalProduct[0] = "Cálcio";
+  chemicalProduct[1] = "Zinco";
+  chemicalProduct[2] = "Hidrazina";
+  chemicalProduct[3] = "Anilina";
+}
+catch (IndexOutOfRangeException ex)
+{
+  Console.WriteLine($"Erro Específico, sabemos o motivo do erro: {ex.Message}");
+}
+catch (Exception ex)
+{
+  Console.WriteLine("Temos a mensagem, porém é um pouco incerto o que ocorreu.");
+}
+
+class Verify
+{
+    public bool VerifyArray(string[] array)
+    {
+        try
+        {
+            if (array == null || array.Length < 5)
+            {
+                throw new ArgumentException("O array é nulo ou possui menos de 5 elementos.");
+            }
+
+            return true;
+        }
+        catch (ArgumentException ex)
+        {
+            Console.WriteLine(ex.Message);
+            throw;
+        }
+    }
+}
+
