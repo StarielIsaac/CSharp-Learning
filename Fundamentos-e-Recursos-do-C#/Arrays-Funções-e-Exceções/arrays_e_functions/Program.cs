9b384795f0a4a4625227986a122994b1087f5da7
Stariel Isaac <starielisaac@gmail.com>
2023-07-04 15:34:33 -0300

    feat: created array type double

--- Fundamentos-e-Recursos-do-C#/Arrays-Funções-e-Exceções/arrays_e_functions/Program.cs
+++ Fundamentos-e-Recursos-do-C#/Arrays-Funções-e-Exceções/arrays_e_functions/Program.cs
@@ -2,12 +2,17 @@
 //por se tratar de uma linguagem Fortemente Tipada, os dados são armazenados de apenas um tipo
 // definido previamente. A vantagem disso é a confiabilidade dos dados no sistema.
 
+//Outro detalhe importante é que o tamanho de um Array em C# é imutável, ou seja,
+// assim que a instância de um Array é criada, o seu tamanho não pode ser alterado.
+
 int[] myFirstArray = {1, 2, 3, 4, 5};
 string[] mySecondArray = {"test1", "test2", "test3"};
+double[] myThirdArray = {123.43, 123.44, 123.45};
 
 Console.WriteLine(myFirstArray[0]);
 Console.WriteLine(mySecondArray[0]);
+Console.WriteLine(myThirdArray[0]);
 
-//Outro detalhe importante é que o tamanho de um Array em C# é imutável, ou seja,
-// assim que a instância de um Array é criada, o seu tamanho não pode ser alterado.
+
+//Para declarar e criar um Array multidimensional:
 
