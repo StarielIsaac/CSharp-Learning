729ae4b242e0b32fc55eefcc30eeaa388ef885eb
Stariel Isaac <starielisaac@gmail.com>
2023-07-04 15:27:36 -0300

    feat: created arrays (int and string)

--- Fundamentos-e-Recursos-do-C#/Arrays-Funções-e-Exceções/arrays_e_functions/Program.cs
+++ Fundamentos-e-Recursos-do-C#/Arrays-Funções-e-Exceções/arrays_e_functions/Program.cs
@@ -1,1 +1,12 @@
-﻿
+﻿// Os Arrays em C# são parecidos com os Arrays em Javascript. O que os diferencia é que, em C#, 
+//por se tratar de uma linguagem Fortemente Tipada, os dados são armazenados de apenas um tipo
+// definido previamente. A vantagem disso é a confiabilidade dos dados no sistema.
+
+int[] myFirstArray = {1, 2, 3, 4, 5};
+string[] mySecondArray = {"test1", "test2", "test3"};
+
+Console.WriteLine(myFirstArray[0]);
+Console.WriteLine(mySecondArray[0]);
+
+//Outro detalhe importante é que o tamanho de um Array em C# é imutável, ou seja,
+// assim que a instância de um Array é criada, o seu tamanho não pode ser alterado.
