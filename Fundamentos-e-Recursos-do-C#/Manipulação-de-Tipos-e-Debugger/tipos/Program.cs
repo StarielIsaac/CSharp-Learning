1212418f59bd19c81ca805ba58894a2284fe9d72
Stariel Isaac <starielisaac@gmail.com>
2023-07-06 19:04:10 -0300

    feat: created method Contains

--- Fundamentos-e-Recursos-do-C#/Manipulação-de-Tipos-e-Debugger/tipos/Program.cs
+++ Fundamentos-e-Recursos-do-C#/Manipulação-de-Tipos-e-Debugger/tipos/Program.cs
@@ -11,6 +11,7 @@ string result = string.Concat(textOne, textTwo, textThree);
 string emails = "email1@trybe.com;email2@trybe.com;email3@trybe.com";
 string[] arrayEmails = emails.Split(";");
 
+
 // IndexOf() - Procurando um index na string
 string trybe = "Trybe";
 int index = trybe.IndexOf("y"); //2
@@ -18,6 +19,23 @@ int index = trybe.IndexOf("y"); //2
 string indexNull = "Trybe";
 int index1 = indexNull.IndexOf("s"); //-1 pois não existe 's' na palavra
 
-// Contains() - 
+
+//Estamos dizendo para a função IndexOf() encontrar o caracter e a partir do index 6.
+string indexNull1 = "I love Trybe";
+int index2 = indexNull1.IndexOf("e", 6); //11
+
+
+// Contains() - Verificar se uma string está contida em outra string, ou em um array.
+List<string> languages = new List<string> 
+{
+ "c#",
+ "java",
+ "javascript",
+ "python"
+};
+
+bool languageExists = languages.Contains("c#"); //retorno um booleano, ou seja, true ou false.
+
+
 // Join() - 
 
