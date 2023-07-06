9c1225ceb78b8cb134c7f24732be543b57c0b60e
Stariel Isaac <starielisaac@gmail.com>
2023-07-06 18:53:34 -0300

    feat: created method IndexOf

--- Fundamentos-e-Recursos-do-C#/Manipulação-de-Tipos-e-Debugger/tipos/Program.cs
+++ Fundamentos-e-Recursos-do-C#/Manipulação-de-Tipos-e-Debugger/tipos/Program.cs
@@ -7,10 +7,17 @@ string textThree = "e agora sabe concatenar textos utilizando a função Concat(
 string result = string.Concat(textOne, textTwo, textThree); 
 
 // Split() - Para separarmos uma string em várias
+// A função Split() retorna um array de strings
 string emails = "email1@trybe.com;email2@trybe.com;email3@trybe.com";
 string[] arrayEmails = emails.Split(";");
 
-// IndexOf() - 
+// IndexOf() - Procurando um index na string
+string trybe = "Trybe";
+int index = trybe.IndexOf("y"); //2
+
+string indexNull = "Trybe";
+int index1 = indexNull.IndexOf("s"); //-1 pois não existe 's' na palavra
+
 // Contains() - 
 // Join() - 
 
