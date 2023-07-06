d445380f9ea74a352c15c38419be0823c3b10dbf
Stariel Isaac <starielisaac@gmail.com>
2023-07-06 18:46:39 -0300

    feat: created method Concat

---
+++ Fundamentos-e-Recursos-do-C#/Manipulação-de-Tipos-e-Debugger/tipos/Program.cs
@@ -0,0 +1,13 @@
+﻿// Funções da Classe String
+
+// Concat() - Concatenação de strings
+string textOne = "Você está aprendendo sobre ";
+string textTwo = "Strings em C#, ";
+string textThree = "e agora sabe concatenar textos utilizando a função Concat()!";
+string result = string.Concat(textOne, textTwo, textThree); 
+
+
+// IndexOf() - 
+// Contains() - 
+// Join() - 
+
