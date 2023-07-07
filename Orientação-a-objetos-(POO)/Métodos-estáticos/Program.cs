1f473e1ea2c462c464115183bc70cf2f00b7ae0e
Stariel Isaac <starielisaac@gmail.com>
2023-07-07 20:34:49 -0300

    feat: implemented gets and sets

---
+++ Orientação-a-objetos-(POO)/Métodos-estáticos/Program.cs
@@ -0,0 +1,17 @@
+﻿class Rocket
+{
+  int _fuel;
+  int _maxFuel = 5000;
+
+  public int FuelAsPercentage
+  {
+    get { return _fuel * 100 / _maxFuel; }
+    set
+    {
+      if (value > 100 || value < 0)
+        throw new ArgumentOutOfRangeException();
+
+      _fuel = (value * _maxFuel) / 100;
+    }
+  }
+}
