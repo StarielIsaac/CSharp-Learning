372ef842d35717c070b8a9d36ada1581f8c045f2
Stariel Isaac <starielisaac@gmail.com>
2023-07-07 21:09:29 -0300

    feat: created class Bakery

--- Orientação-a-objetos-(POO)/Métodos-estáticos/Program.cs
+++ Orientação-a-objetos-(POO)/Métodos-estáticos/Program.cs
@@ -15,3 +15,48 @@
     }
   }
 }
+
+class Bakery
+{
+    public Bread fabricateBread(int weight)
+    {
+        return new Bread
+        {
+            Weight = weight,
+            Type = "White"
+        };
+    }
+
+    public Cake fabricateChocolateCake(string size)
+    {
+        return new Cake
+        {
+            Size = size,
+            Flavour = "Chocolate"
+        };
+    }
+}
+
+class Bread
+{
+    string _type = "";
+
+    public int Weight { get; set; }
+    public string Type
+    {
+        get { return _type; }
+        set
+        {
+            if (value != "White" && value != "Wheat")
+                throw new ArgumentException("Invalid bread type");
+
+            _type = value;
+        }
+    }
+}
+
+class Cake
+{
+    public string Size { get; set; }
+    public string Flavour { get; set; }
+}
